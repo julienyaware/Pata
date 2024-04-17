148a2e344c90d2eaa4e2a093a68151e25cd50d89
Julliet Nyaware <julienyaware@gmail.com>
2024-04-16 22:26:43 -0400

    Fixed some styling on categories and providers

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { categories } from './data/categories';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
    
     return (
-        <div className="max-w-[1640px] m-auto px-10 py-12  bg-white h-screen w-screen">
+        <div className="max-w-[1640px] m-auto px-20 py-12  bg-white h-screen w-screen">
       <h1 className="font-bold text-black text-4xl text-center">
         CATEGORIES
       </h1>
       {/* Categories */}
+      <Link
+       to="/providers"
+                        >
       <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
         {categories.map((item, index) => (
           <div
@@ -20,6 +24,7 @@ const Categories = () => {
           </div>
         ))}
       </div>
+      </Link>
     </div>
     );
 };
